3a876789943d3fdc66f87c0de3e9319185edac03
pablosg-5 <pablosangom5@gmail.com>
2025-01-10 20:47:35 +0100

    #1 Import photos

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sevillatatis</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <header>
+    <!-- Logo, menú de navegación -->
+     <h1>Hola buenos</h1>
+  </header>
+  <main>
+    <!-- Secciones principales -->
+  </main>
+  <footer>
+    <!-- Información de contacto, enlaces rápidos -->
+  </footer>
+</body>
+</html>
