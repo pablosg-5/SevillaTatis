01c7f4b0ad22b61ca3a2ac8b9fac8492d6e97d43
pablosg-5 <pablosangom5@gmail.com>
2025-01-11 14:27:02 +0100

    Payment simulation #1

---
+++ public/proceso_pago.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-style=1.0">
+    <title>Simulación de Pago</title>
+    <link rel="stylesheet" href="styles/pago.css">
+</head>
+
+<body>
+    <h1>Procesando Pago...</h1>
+    <script>
+        setTimeout(function() {
+            window.location.href = "profile.php"; // Redirige después de 3 segundos
+        }, 3000); // 3000 ms = 3 segundos
+    </script>
+</body>
+
+</html>
